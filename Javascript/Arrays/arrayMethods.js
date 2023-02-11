6e8fa2fdb9ef63b0e3cf6a8692804eaa0a6e362c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-11 12:48:49 +0200

    added desc. of array methods

--- Javascript/Arrays/arrayMethods.js
+++ Javascript/Arrays/arrayMethods.js
@@ -25,3 +25,22 @@ let numbers = [1,2,3,4];
 
 // //INCLUDES
 // console.log(numbers.includes(48));
+
+
+
+
+////////////////////////// ARRAY METHODS DESCRIPTIONS //////////////////////////
+
+// push(): adds one or more elements to the end of an array and returns the new length.
+// pop(): removes the last element of an array and returns that element.
+// shift(): removes the first element from an array and returns that element.
+// unshift(): adds one or more elements to the beginning of an array and returns the new length.
+// splice(): changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+// slice(): returns a new array containing a portion of the original array.
+// concat(): merges two or more arrays and returns a new array that contains all the elements.
+// includes(): determines whether an array includes a certain value among its entries.
+// indexOf(): returns the first index at which a given element can be found in the array, or -1 if it is not present.
+// forEach(): executes a provided function once for each array element.
+// map(): creates a new array with the results of calling a provided function on every element in the calling array.
+// filter(): creates a new array with all elements that pass the test implemented by the provided function.
+// reduce(): executes a reducer function on each element of the array, resulting in a single output value.
