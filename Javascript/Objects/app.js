e5934008aea1e4b2efe719304f38744802c7d981
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-15 09:28:57 +0200

    checking factory functions

--- Javascript/Objects/app.js
+++ Javascript/Objects/app.js
@@ -1,14 +1,37 @@
-const circle = {
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
-    invisible: true,
+///////////////////////////////
+// Factory functions        //
+/////////////////////////////
 
-    draw: function (){
-        console.log("circle");
-    }
-    };
 
-    circle.draw();
+function circle (radius) {
+    return {
+        radius,
+        draw (){
+            console.log("circleee");
+        }
+        };
+}
+  
+  const circle1 = circle(123);
+  circle1.draw();
+  console.log(circle1);
+  
+
+///////////////////////////////
+// Objects   - Method       //
+/////////////////////////////
+
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x:1,
+//         y:1
+//     },
+//     invisible: true,
+
+//     draw: function (){
+//         console.log("circle");
+//     }
+//     };
+
+//     circle.draw();
