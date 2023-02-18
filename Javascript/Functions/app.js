dee63ffe2bf465a6b7cb7fc88b604d3b39db6b89
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-18 12:09:05 +0200

    function declarations function expressions and hoisting test

--- Javascript/Functions/app.js
+++ Javascript/Functions/app.js
@@ -1,20 +1,33 @@
-// Function Declaration 
-function speak (){
-    console.log("hello");
-}
+// // Function Declaration 
+// function speak (){
+//     console.log("hello");
+// }
 
-// Named Function Expression
-let conveNamed = function talk () {
-    console.log("bla bla bla")
-};
+// // Named Function Expression
+// let conveNamed = function talk () {
+//     console.log("bla bla bla")
+// };
 
-// Anonymous Function Expression
-let conveAnon = function () {
-    console.log("bla bla bla")
-};
+// // Anonymous Function Expression
+// let conveAnon = function () {
+//     console.log("bla bla bla")
+// };
 
 //Variables here are reference points for functions
 //Declarations can be Hoisted
 //Expressions can NOT be Hoisted
 
-// JS moves all Function Declarations to the - top.
+// JS moves all Function Declarations  to the - top.
+
+
+
+
+
+
+
+
+
+
+
+
+
