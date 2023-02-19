9fac23f3f9dbd529385421484f1f9beb0567055f
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-19 18:37:22 +0200

    experimenting making lame photo galery

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,13 +1,14 @@
-let obj = {
-    name: "domantas",
-    last: "balciunas",
-    age: 13,
-    games: "COD, STEAM, Origin"
-}
-
-let clone = {...obj};
+const img = document.querySelector("#gallery");
+const nextBtn = document.querySelector("#next");
 
 
-console.log(obj);
+const photos = ["1.jpg","2.jpg","3.jpg"];
 
-console.log(clone);
+let counter = 0;
+
+nextBtn.addEventListener("click", () => {
+    counter = (counter + 1) % photos.length;
+    img.style.background = "url" + "(" + photos[counter] + ")";
+    img.style.objectFit = "contain";
+    img.style.backgroundSize = "cover";
+})
