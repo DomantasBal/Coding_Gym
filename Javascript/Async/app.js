1b82897badf6db5c7e11437d08bbafd04c55e085
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-21 17:53:57 +0200

    21

--- Javascript/Async/app.js
+++ Javascript/Async/app.js
@@ -1,22 +1,19 @@
 // fetch("https://jsonplaceholder.typicode.com/todos/1")
 // .then(res => console.log(res))
 
+// function getData (){
+//     return new Promise ((resolve, reject)=>{
+//         // resolve ("Done")
+//         reject("Failed")
+//     })
+// }
 
+// function onSuccess(data){
+//     console.log(`Progress ${data}` )
+// }
 
+// function onError(data){
+//     console.log(`progress ${data}`)
+// }
 
-function getData (){
-    return new Promise ((resolve, reject)=>{
-        // resolve ("Done")
-        reject("Failed")
-    })
-}
-
-function onSuccess(data){
-    console.log(`Progress ${data}` )
-}
-
-function onError(data){
-    console.log(`progress ${data}`)
-}
-
-getData().then(onSuccess,onError);
+// getData().then(onSuccess,onError);
