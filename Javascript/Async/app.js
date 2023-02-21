61081cb4bbc98da756ba56b62f9a426b277e8ec6
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-21 15:06:53 +0200

    checking Promises and .then

---
+++ Javascript/Async/app.js
@@ -0,0 +1,22 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then(res => console.log(res))
+
+
+
+
+function getData (){
+    return new Promise ((resolve, reject)=>{
+        // resolve ("Done")
+        reject("Failed")
+    })
+}
+
+function onSuccess(data){
+    console.log(`Progress ${data}` )
+}
+
+function onError(data){
+    console.log(`progress ${data}`)
+}
+
+getData().then(onSuccess,onError);
--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,14 +1,15 @@
-const img = document.querySelector("#gallery");
-const nextBtn = document.querySelector("#next");
+// const img = document.querySelector("#gallery");
+// const nextBtn = document.querySelector("#next");
 
 
-const photos = ["1.jpg","2.jpg","3.jpg"];
+// const photos = ["1.jpg","2.jpg","3.jpg"];
 
-let counter = 0;
+// let counter = 0;
 
-nextBtn.addEventListener("click", () => {
-    counter = (counter + 1) % photos.length;
-    img.style.background = "url" + "(" + photos[counter] + ")";
-    img.style.objectFit = "contain";
-    img.style.backgroundSize = "cover";
-})
+// nextBtn.addEventListener("click", () => {
+//     counter = (counter + 1) % photos.length;
+//     img.style.background = "url" + "(" + photos[counter] + ")";
+//     img.style.objectFit = "contain";
+//     img.style.backgroundSize = "cover";
+// })
+
