2d98c64b5d32292c8609711449cc8201015f1f70
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-26 12:28:49 +0200

    promises

--- Javascript/Promises/app.js
+++ Javascript/Promises/app.js
@@ -2,12 +2,13 @@
 //            Promises             //
 ////////////////////////////////////
 
-let p = new Promise ((resolve, reject) => {
-    let check = 1;
+// let p = new Promise ((resolve, reject) => {
+//     let check = 1;
 
-    if (check === 1){
-        resolve("YES")
-    }else{
-        reject("NO");
-    }
-})
+//     if (check === 1){
+//         resolve (console.log("YES"))
+//     }else{
+//         reject(console.log("NO"));
+//     }
+// })
+
