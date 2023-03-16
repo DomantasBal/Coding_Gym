ac118d25ee882e93e0e0f63a22e6115624dd2c4d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-16 19:27:12 +0200

    checking object and arrays destructuring

---
+++ Javascript/Destructuring/app.js
@@ -0,0 +1,18 @@
+// let { PI } = Math;
+// console.log(PI);
+
+const robot = {
+  name: "Walee",
+  Hobbies: "Collecting garbage",
+  wheels: 8,
+  hands: 2,
+  LovesTech: true,
+};
+
+let keys = Object.keys(robot);
+let values = Object.values(robot);
+let entries = Object.entries(robot);
+
+console.log(keys);
+console.log(values);
+console.log(entries);
