e5f3c7b92456a3305804e0dac57a5f5ee10de18d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-31 15:31:13 +0300

    learning react

--- React/react-first/src/App.js
+++ React/react-first/src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function Header() {
-  return <h1>Heleu!!</h1>;
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
+import Nav from "./components/Nav";
+import Promo from "./components/Promo";
+
+function HomePage() {
+  return (
+    <div>
+      <Nav />
+      <Intro />
+      <Promo />
+      <Footer />
+    </div>
+  );
 }
 function App() {
-  return <Header />;
+  return <HomePage />;
 }
 
 export default App;
