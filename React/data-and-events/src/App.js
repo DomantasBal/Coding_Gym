762c88d6776fb325861de8c7219aa6ffc8ba56f2
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-04-13 12:40:24 +0300

    hooks and states

--- React/data-and-events/src/App.js
+++ React/data-and-events/src/App.js
@@ -4,9 +4,14 @@ import "./App.css";
 
 function App() {
   const [word, setWord] = React.useState("Say");
+
+  function handeClick() {
+    setWord("Drink");
+  }
   return (
     <div className="App">
       <Heading message={word + "Something"} />
+      <button onClick={handeClick}>Click here</button>
     </div>
   );
 }
