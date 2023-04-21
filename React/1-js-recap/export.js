054cdf485570a65ee41837070773ff1871e8b945
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-04-21 18:57:08 +0300

    recap on js before react

---
+++ React/1-js-recap/export.js
@@ -0,0 +1,15 @@
+// IMPORT EXPORT RECAP
+
+const person = {
+  name: 'Domantas',
+};
+
+export default person;
+
+// Exporting function
+export const sing = () => {
+  console.log('Last christmas..');
+};
+
+// Exporting variable
+export const baseData = 10;
---
+++ React/1-js-recap/import.js
@@ -0,0 +1,3 @@
+import person, { sing, baseData } from './export.js';
+
+console.log(person, sing(), baseData);
