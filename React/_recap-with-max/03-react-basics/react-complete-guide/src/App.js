b8b0fe18196305c22bec7e91f41900b8fff43e7c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-04 14:33:26 +0300

    react recap hooks

--- React/_recap-with-max/03-react-basics/react-complete-guide/src/App.js
+++ React/_recap-with-max/03-react-basics/react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -25,19 +26,12 @@ const App = () => {
     },
   ];
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
