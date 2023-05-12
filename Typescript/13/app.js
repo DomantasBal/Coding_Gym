6e1e1fee6024ae2f305bcb2ca44b4651f70224b5
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-12 19:04:56 +0300

    typescript inference

--- Typescript/13/app.js
+++ Typescript/13/app.js
@@ -1,14 +1,21 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+"use strict";
+// function add(n1:number, n2:number, showResult: boolean, phrase: string){
+//     const result = n1 + n2;
+//     if (showResult){
+//         console.log(phrase + result);
+//     }else{
+//         return result;
+//     }
+// }
+// const number1 = 5;
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = 'Result is: ';
+// add(number1, number2, printResult, resultPhrase);
+// Type inference
+let myName = 'Dom';
+let year;
+year = 123;
+// year = "string"
+console.log(typeof myName);
+console.log(typeof year);
