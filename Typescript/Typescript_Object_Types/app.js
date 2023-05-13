e829a7135954fd26bff3fad122b46b65ca8457b4
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-13 19:48:43 +0300

    typescript touples

--- Typescript/Typescript_Object_Types/app.js
+++ Typescript/Typescript_Object_Types/app.js
@@ -1,9 +1,28 @@
 "use strict";
 // Just testing object types. This is not good practice.
 // Better practice is to leave type inference for objects
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: "Larry",
+//     age: 43
+// }
+// console.log(typeof person.age);
+// person.name = 123;
 const person = {
     name: "Larry",
-    age: 43
+    age: 43,
+    hobbies: ['Music', 'Food'],
+    role: [2, 'dev']
 };
-console.log(typeof person.age);
-// person.name = 123;
+// .push() - IS AN EXCEPTION! 
+person.role.push("last item"); // is allowed
+// person.role[1] = 33 // This is not allowed
+let favActivities;
+// You get an error because its not an ARRAY
+// favActivities = "sports";
+for (let hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // ERRORRRRRRRRRRRRR
+}
--- Typescript/Typescript_Object_Types/app.ts
+++ Typescript/Typescript_Object_Types/app.ts
@@ -1,13 +1,48 @@
 
 // Just testing object types. This is not good practice.
 // Better practice is to leave type inference for objects
+
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: "Larry",
+//     age: 43
+// }
+// console.log(typeof person.age);
+// person.name = 123;
+
 const person: {
-    name: string;
-    age: number;
+    name: string,
+    age: number,
+    hobbies: string [],
+    role: [number, string] // ### TOUPLE ##
+    // Tuples
+    // - Is a fixed length and type array. It locks data types to indexes
+
 } = {
     name: "Larry",
-    age: 43
+    age: 43,
+    hobbies: ['Music', 'Food'],
+    role: [2, 'dev']
 }
 
-console.log(typeof person.age);
-// person.name = 123;
+
+// .push() - IS AN EXCEPTION! 
+person.role.push("last item"); // is allowed
+// person.role[1] = 33 // This is not allowed
+
+
+
+
+let favActivities: string [];
+// You get an error because its not an ARRAY
+// favActivities = "sports";
+
+for(let hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // ERRORRRRRRRRRRRRR
+}
+
+
+
