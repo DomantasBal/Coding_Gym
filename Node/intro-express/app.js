3f5a126bf6a10016ccd31766d5ef8dc517ea110b
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-18 12:45:57 +0300

    learning node

--- Node/intro-express/app.js
+++ Node/intro-express/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello world!!!!");
-// });
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course 1" },
@@ -13,6 +11,22 @@ const courses = [
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 bad request
+    res.status(400).send("Name is required and should be minimum 3 chr.");
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
 });
 
 app.get("/api/courses/:id", (req, res) => {
