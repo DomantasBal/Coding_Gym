249370a32b0dd823f31510cf74bca41642fbd328
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-18 13:05:55 +0300

    learning node

--- Node/intro-express/app.js
+++ Node/intro-express/app.js
@@ -15,16 +15,9 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  const result = Joi.validate(req.body, schema);
-  console.log(result);
-
-  if (!req.body.name || req.body.name.length < 3) {
-    // 400 bad request
-    res.status(400).send("Name is required and should be minimum 3 chr.");
+  const { error } = validateCourse(req.bodu);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -36,6 +29,33 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // if not existing return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course id is invalid");
+
+  const { error } = validateCourse(req.body); // result.error
+  if (error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  //update course
+  course.name = req.body.name;
+  res.send(course);
+  //return the updated course
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(course, schema);
+}
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
