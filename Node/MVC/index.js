d94d8068a323d604af3fb3bea49db06e520906b4
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-30 11:11:18 +0300

    revising MVC

---
+++ Node/MVC/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const userRoute = require("./routes/User");
+
+app.use("/user", userRoute);
+
+app.listen(3005, () => console.log(`server is running `));
