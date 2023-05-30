5e369901f385396b10c83531a831f7a4a3058476
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-30 13:36:28 +0300

    revising query params

--- Node/query-params/index.js
+++ Node/query-params/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res) => console.log(req.query));
+app.get("/", (req, res) => console.log(req.query));
 
-app.get("/person", (req, res) => {
-  console.log(req.query);
-  return res.json({
-    message: "heyyy",
-    name: req.query.name,
-    age: req.query.age,
-  });
-});
+// app.get("/person", (req, res) => {
+//   console.log(req.query);
+//   return res.json({
+//     message: "heyyy",
+//     name: req.query.name,
+//     age: req.query.age,
+//   });
+// });
 
 app.listen(3000, () => console.log("server is running "));
